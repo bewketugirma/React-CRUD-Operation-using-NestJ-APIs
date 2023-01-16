4f1bc8a17ea3fdadd12311f5db49c351f36689e0
girma <34367034+bewketugirma@users.noreply.github.com>
2023-01-16 16:17:09 +0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Layout from "./components/shared/Layout";
+ import reportWebVitals from './reportWebVitals';
+
+function App() {
+  return (
+    
+    <Layout>
+    
+    </Layout>
+  );
+}
+reportWebVitals();
+export default App;
